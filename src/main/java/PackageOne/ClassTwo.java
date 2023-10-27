29cada1e149e4a5cc0bf52c75d3be6056ed1ecf3
Dolfis Kareth <kdolfis@gmail.com>
2023-10-27 07:32:05 +0700

    Membuat output "Hello World"

--- src/main/java/PackageOne/ClassTwo.java
+++ src/main/java/PackageOne/ClassTwo.java
@@ -3,5 +3,6 @@ package PackageOne;
 public class ClassTwo {
     public static void main(String[] args) {
         System.out.println("Class Two");
+        System.out.println("Hello World");
     }
 }
