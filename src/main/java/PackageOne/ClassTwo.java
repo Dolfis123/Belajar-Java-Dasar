249282800db56cf362e5fac4f3414cb0b7620581
Dolfis Kareth <kdolfis@gmail.com>
2023-10-27 07:34:28 +0700

    Import Scanner ke Class Two

--- src/main/java/PackageOne/ClassTwo.java
+++ src/main/java/PackageOne/ClassTwo.java
@@ -1,8 +1,11 @@
 package PackageOne;
 
+import java.util.Scanner;
+
 public class ClassTwo {
     public static void main(String[] args) {
         System.out.println("Class Two");
         System.out.println("Hello World");
+        Scanner scan = new Scanner(System.in);
     }
 }
