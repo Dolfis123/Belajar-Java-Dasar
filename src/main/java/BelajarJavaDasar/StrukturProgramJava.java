167a84d16713ea3deaef212e31ec138cd720e267
Dolfis Kareth <kdolfis@gmail.com>
2023-11-01 20:11:30 +0700

    Create an class struktur program java

---
+++ src/main/java/BelajarJavaDasar/StrukturProgramJava.java
@@ -0,0 +1,10 @@
+package BelajarJavaDasar; //ini adalah Package
+//Ini adalah Classs, Wadah, Blue_Print dari kelas ini
+public class StrukturProgramJava { //Body Class Start
+    //    Ini adalah main funtion, program yang pertama kali di jalankan
+    public static void main(String[] args){  //Body Main Funtion Start
+
+
+    }//Body Mian Funtion End
+}//Body Class End
+
